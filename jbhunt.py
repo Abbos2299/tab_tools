4c13470364bc1da0d77739f96767d845aa2d5e18
Abbos <abboskhon.kh@gmail.com>
2023-08-24 19:14:13 +0300

    Update jbhunt.py

--- jbhunt.py
+++ jbhunt.py
@@ -10,6 +10,8 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+timestamp = datetime.now().strftime("%y%m%d %H%M %S")
+
 # Initialize Firebase credentials
 cred = credentials.Certificate('tab-tools-firebase-adminsdk-8ncav-4f5ccee9af.json')
 firebase_admin.initialize_app(cred)
