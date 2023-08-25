eef6243bd667e0c93bfa5f600bcc786653b7c10b
Abbos <abboskhon.kh@gmail.com>
2023-08-25 11:26:29 +0300

    Update broker_identify.py

--- broker_identify.py
+++ broker_identify.py
@@ -18,7 +18,6 @@ app = Flask(__name__)
 cred = credentials.Certificate('tab-tools-firebase-adminsdk-8ncav-4f5ccee9af.json')
 firebase_admin.initialize_app(cred)
 
-
 @app.route('/launch', methods=['GET'])
 def launch_python_file():
     user_uid = request.args.get('uid')
