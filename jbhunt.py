3da6f60eca80a5d29da63e954eb38d32c983b385
Abbos <abboskhon.kh@gmail.com>
2023-08-26 17:32:59 +0300

    Update jbhunt.py

--- jbhunt.py
+++ jbhunt.py
@@ -26,9 +26,8 @@ file_name = sys.argv[3]
 def extract_text_from_pdf(file_path):
     resource_manager = PDFResourceManager()
     output_stream = io.StringIO()
-    codec = 'utf-8'
     laparams = pdfminer.layout.LAParams()
-    device = TextConverter(resource_manager, output_stream, codec=codec, laparams=laparams)
+    device = TextConverter(resource_manager, output_stream, laparams=laparams)
     with open(file_path, 'rb') as file:
         interpreter = PDFPageInterpreter(resource_manager, device)
         for page in PDFPage.get_pages(file):
