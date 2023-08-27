10ac7d8830ddb5df39f67d61a950d0e116265fd1
Abbos <abboskhon.kh@gmail.com>
2023-08-27 20:05:43 +0300

    Update jbhunt.py

--- jbhunt.py
+++ jbhunt.py
@@ -98,6 +98,7 @@ def save_result_to_firebase(load_number, rate, broker_email, load_miles, pick_up
         'PickUpTime': pick_up_t,
         'Deliveries': consignee_location,
         'DeliveryTimes': delivery_times,
+        'Status': 'Active',
     })
     
 # Extract text from the PDF
