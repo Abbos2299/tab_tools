6a028125419838c8ab5dbafa812d9a580dd3a695
Abbos <abboskhon.kh@gmail.com>
2023-10-08 14:11:50 +0300

    Update impira.py

--- impira.py
+++ impira.py
@@ -1,13 +1,25 @@
 from docquery import document, pipeline
-p = pipeline('document-question-answering')
+
+# Load the document from the PDF file
 doc = document.load_document("8B49232.pdf")
-for q in ["What is the load number?",
-          "What is broker email?",
-          "What is Pick up location?",
-          "What is Pick up date?",
-          "What is the Delivery location?",
-          "What is Delivery date?",
-          "What is the Total amount?"]:
+
+# Initialize the question-answering pipeline
+p = pipeline('document-question-answering')
+
+# List of questions to ask
+questions = [
+    "What is the load number?",
+    "What is broker email?",
+    "What is Pick up location?",
+    "What is Pick up date?",
+    "What is the Delivery location?",
+    "What is Delivery date?",
+    "What is the Total amount?"
+]
+
+# Loop through the questions and print the answers
+for q in questions:
     result = p(question=q, **doc.context)
     answer = result[0]['answer'] if result else 'No answer found'
-    print(answer)
+    print(f"Question: {q}")
+    print(f"Answer: {answer}\n")
