f54760911796240391805e9d6396f3ba5163912d
Abbos <abboskhon.kh@gmail.com>
2023-10-08 15:02:46 +0300

    Update impira.py

--- impira.py
+++ impira.py
@@ -1,25 +1,5 @@
 from docquery import document, pipeline
-
-# Load the document from the PDF file
-doc = document.load_document("8B49232.pdf")
-
-# Initialize the question-answering pipeline
 p = pipeline('document-question-answering')
-
-# List of questions to ask
-questions = [
-    "What is the load number?",
-    "What is broker email?",
-    "What is Pick up location?",
-    "What is Pick up date?",
-    "What is the Delivery location?",
-    "What is Delivery date?",
-    "What is the Total amount?"
-]
-
-# Loop through the questions and print the answers
-for q in questions:
-    result = p(question=q, **doc.context)
-    answer = result[0]['answer'] if result else 'No answer found'
-    print(f"Question: {q}")
-    print(f"Answer: {answer}\n")
+doc = document.load_document("https://templates.invoicehome.com/invoice-template-us-neat-750px.png")
+for q in ["What is the invoice number?", "What is the invoice total?"]:
+    print(q, p(question=q, **doc.context))
