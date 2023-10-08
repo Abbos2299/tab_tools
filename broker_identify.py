e8565eec583bbbf83cd70882b6f74a435afba382
Abbos <abboskhon.kh@gmail.com>
2023-10-08 11:57:37 +0300

    Update broker_identify.py

--- broker_identify.py
+++ broker_identify.py
@@ -1,19 +1,18 @@
 import subprocess
 import sys
+import tempfile
 from flask import Flask, request
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import firestore
 from firebase_admin import storage
-from datetime import timedelta
-import requests
 import time
-import urllib.parse
 import os
-import pytesseract
-from pdf2image import convert_from_path
-import re
-from docquery import document, pipeline
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+import io
+
 
 app = Flask(__name__)
 cred = credentials.Certificate(
@@ -32,72 +31,65 @@ def launch_python_file():
     blobs = bucket.list_blobs(prefix=folder_name)
 
     time.sleep(1)
-
-    last_added_blob = None
     for blob in blobs:
-        if not last_added_blob or blob.updated > last_added_blob.updated:
-            last_added_blob = blob
+        if blob.name.lower().endswith('.pdf'):
+            # Create a temporary file to save the PDF content
+            with tempfile.NamedTemporaryFile(suffix=".pdf") as temp_pdf_file:
+                # Download the PDF content to the temporary file
+                blob.download_to_filename(temp_pdf_file.name)
 
-    if last_added_blob:
-        file_name = urllib.parse.unquote(last_added_blob.name.split(
-            '/')[-1])  # Get the file name from the blob URL
-        file_url = last_added_blob.generate_signed_url(
-            expiration=timedelta(minutes=15))
+                # Extract text from the PDF using pdfminer
+                text = extract_text_from_pdf(temp_pdf_file.name)
 
-        # Download the file from Firebase
-        response = requests.get(file_url)
-        with open(file_name, 'wb') as f:
-            f.write(response.content)
-
-        images = convert_from_path(file_name)
-        all_text = ""
-        for i, image in enumerate(images):
-            text = pytesseract.image_to_string(image)
-            all_text += text
-        broker_companies = [
-            "J.B. Hunt",
-        ]
-
-        # Initialize a dictionary to count the occurrences of each broker company
-        broker_counts = {company: 0 for company in broker_companies}
-
-        # Search for matches with broker companies
-        for company in broker_companies:
-            matches = re.findall(r'\b' + re.escape(company) +
-                                 r'\b', all_text, flags=re.IGNORECASE)
-            count = len(matches)
-            broker_counts[company] = count
-
-        # Find the most used broker company
-        most_used_broker = max(broker_counts, key=broker_counts.get)
-
-       
+                # Process the extracted text as needed
+                # You can replace this with your logic
+                if most_used_broker == "J.B. Hunt":
+                    most_used_broker = "J. B. Hunt Transportation"
+                    subprocess.call([sys.executable, "jbhunt.py",
+                                     user_uid, most_used_broker])
+                else:
+                    print("RC didn't identify")
+                    subprocess.call([sys.executable, "impira.py",
+                                     user_uid])
 
 
-
-    if broker_counts[most_used_broker] > 0:
-        print(most_used_broker)
 
     # Correct the identified broker company names if needed
-        if most_used_broker == "J.B. Hunt":
-            most_used_broker = "J. B. Hunt Transportation"
-            subprocess.call([sys.executable, "jbhunt.py",
-                        user_uid, most_used_broker, file_name])
-        else:
-            print("RC don't identified")
-            subprocess.call([sys.executable, "impira.py",
-                     user_uid, file_name])
-
-        # Wait for 1 seconds
-        time.sleep(1)
-        # Delete the file
-        os.remove(file_name)
-
+    if most_used_broker == "J.B. Hunt":
+        most_used_broker = "J. B. Hunt Transportation"
+        subprocess.call([sys.executable, "jbhunt.py",
+                         user_uid, most_used_broker])
     else:
-        print('No files found in the folder')
+        print("RC don't identified")
+        subprocess.call([sys.executable, "impira.py",
+                         user_uid])
 
     return 'Success'
 
+def extract_text_from_pdf(pdf_path):
+    """
+    Extract text from a PDF file using pdfminer.
+    """
+    rsrcmgr = PDFResourceManager()
+    retstr = io.StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    with open(pdf_path, 'rb') as fp:
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        password = ""
+        maxpages = 0
+        caching = True
+        pagenos = set()
+
+        for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=True):
+            interpreter.process_page(page)
+
+    text = retstr.getvalue()
+    device.close()
+    retstr.close()
+    return text
 
 @app.route('/locationcheck', methods=['GET'])
 def location_check():
