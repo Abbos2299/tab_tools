4ef6dcb9e2e48bf9b0ed86f923c9f4b40bfa412f
Abbos <abboskhon.kh@gmail.com>
2024-01-08 12:00:22 +0200

    Delete impira.py

--- impira.py
+++
@@ -1,5 +0,0 @@
-from docquery import document, pipeline
-p = pipeline('document-question-answering')
-doc = document.load_document("https://templates.invoicehome.com/invoice-template-us-neat-750px.png")
-for q in ["What is the invoice number?", "What is the invoice total?"]:
-    print(q, p(question=q, **doc.context))
